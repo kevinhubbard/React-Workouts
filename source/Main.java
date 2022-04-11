ee0f083a6008da2903b4dc52c7b8170b5a225683
Kevin H <dgkallday98@gmail.com>
2022-04-11 19:10:24 -0400

    panel updates

--- source/Main.java
+++ source/Main.java
@@ -1,16 +1,33 @@
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class Main {
 
+	public static JMenuBar createMenu() {
+		JMenuBar jmb = new JMenuBar();
+		JMenu file = new JMenu("File");
+		JMenuItem nmi = new JMenuItem("New Workout");
+		JMenuItem vmi = new JMenuItem("View Workout");
+		JMenuItem emi = new JMenuItem("Exit");
 
+		emi.addActionListener(new MenuListener());
+
+		file.add(nmi);
+		file.add(vmi);
+		file.add(emi);
+
+		jmb.add(file);
+		return jmb;
+	}
 
 	public static void guiSetup() {
-			 JFrame frame = new JFrame("Workouts");
-	 JPanel panel = new JPanel();
-	 JButton addBtn = new JButton("Add");
+		JFrame frame = new JFrame("Workouts");
+	 	JPanel panel = new JPanel();
+	 	JButton addBtn = new JButton("Add");
 		panel.add(addBtn);
 		frame.setSize(new Dimension(500, 500));
+		frame.setJMenuBar(createMenu());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().add(BorderLayout.NORTH, panel);
 		frame.setVisible(true);
@@ -18,5 +35,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		guiSetup();
+	}
+
+	private static class MenuListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("shit was clicked yo.");
+		}
 	}
 }
---
+++ source/ViewPanel.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class ViewPanel extends JPanel {
+	
+	//Constructor
+	public ViewPanel() {
+	
+	}
+}
